a579eb877d4553ab0f3bac33cb8295d9acb2843d
Ali Haghighatkhah <ali@lightcurve.io>
2022-05-17 22:48:08 +0200

    Remove old ones

--- src/app/modules.ts
+++
@@ -1,7 +0,0 @@
-/* eslint-disable @typescript-eslint/no-empty-function */
-import { Application } from 'lisk-sdk';
-import { LnsModule } from "./modules/lns/lns_module";
-
-export const registerModules = (app: Application): void => {
-    app.registerModule(LnsModule);
-};
--- src/app/plugins.ts
+++
@@ -1,4 +0,0 @@
-/* eslint-disable @typescript-eslint/no-empty-function */
-import { Application } from 'lisk-sdk';
-
-export const registerPlugins = (app: Application): void => {};
